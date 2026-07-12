cb749abf2faba25e327564d09704f81424db6205
juanjoxdll <juanjosehleal@gmail.com>
2026-07-11 20:53:13 -0600

    Add files via upload
    Agregar selector entre versiones 1.5.1 y 2.0

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="mis-turnos-v1-5-1";
-const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE="mis-turnos-v2-0";
+const FILES=["./","./index.html","./clasica-v1.5.1.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match("./index.html"))))});
