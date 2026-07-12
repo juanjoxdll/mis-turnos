c9fe612dad753ceb493edf9fa1bb5e5adbba80e7
juanjoxdll <juanjosehleal@gmail.com>
2026-07-11 23:39:07 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="mis-turnos-v2-0-4";
+const CACHE="mis-turnos-v2-5-stable";
 const FILES=["./","./index.html","./clasica-v1.5.1.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
